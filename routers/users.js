5422762bc6121bab932f52cd8cf410e9050b3d54
Alexandr <colmoon@ya.ru>
2020-09-23 15:28:34 +0300

    fix path

--- routers/users.js
+++ routers/users.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
 const { userInfo } = require('../controllers/users');
 
-router.get('/user/me', celebrate({
+router.get('/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30).required(),
   }),
