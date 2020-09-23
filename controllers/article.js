ea706e4b5a7a29651fc8d0d66cc1117853d07e4b
Alexandr <colmoon@ya.ru>
2020-09-23 16:41:17 +0300

    add postArticle

--- controllers/article.js
+++ controllers/article.js
@@ -11,7 +11,22 @@ const getArticles = (req, res, next) => {
 };
 
 const postArticle = (req, res, next) => {
-
+  const {
+    keyword, title, text, date, source, link,
+    image,
+  } = req.body;
+  Article.create({
+    keyword,
+    title,
+    text,
+    date,
+    source,
+    link,
+    image,
+    owner: req.user._id,
+  })
+    .then((article) => res.status(201).send({ data: article }))
+    .catch(next);
 };
 
 module.exports = {
