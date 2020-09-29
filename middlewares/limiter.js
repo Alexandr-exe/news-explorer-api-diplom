12ea323c47d2bf710c7605f13c2aae79b54d4940
Alexandr <colmoon@ya.ru>
2020-09-29 13:59:37 +0300

    fix code

--- middlewares/limiter.js
+++ middlewares/limiter.js
@@ -1,7 +1,7 @@
 const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // за 15 минут
-  max: 10000, // можно совершить максимум 10000 запросов с одного IP
+  windowMs: 15 * 60 * 1000,
+  max: 10000,
 });
 module.exports = { limiter };
