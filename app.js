1f0558be6ba8a54858c47a7aa91ac5411311f7cd
Alexandr <colmoon@ya.ru>
2020-10-05 17:34:22 +0300

    fix code

--- app.js
+++ app.js
@@ -5,91 +5,45 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
-const { celebrate, Joi, errors } = require('celebrate');
+const { errors } = require('celebrate');
 
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const auth = require('./middlewares/auth');
+
 const { limiter } = require('./middlewares/limiter');
 
-const { createUser, login } = require('./controllers/users');
-const users = require('./routers/users');
-const articles = require('./routers/articles');
 const { PORT, DATABASE_URL } = require('./config');
 
+const router = require('./routers/index');
+const errorHandler = require('./middlewares/errorHandler');
 const NotFoundError = require('./errors/NotFoundError');
 
 const app = express();
+
 app.use(cookieParser());
 
 app.use(helmet());
 
-async function start() {
-  try {
-    await mongoose.connect(DATABASE_URL, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true,
-    });
-    app.listen(PORT);
-  } catch (e) {
-    console.log(e);
-  }
-}
+mongoose.connect(DATABASE_URL, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
 app.use(requestLogger);
-
 app.use(limiter);
-app.post('/signup', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().min(2).max(30).required(),
-    email: Joi.string().email().required(),
-    password: Joi.string().trim().min(5).required(),
-  }),
-}), createUser);
 
-app.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().email().required(),
-    password: Joi.string().trim().min(5).required(),
-  }),
-}), login);
+app.use(router);
 
-app.use(auth);
-
-app.use('/users', users);
-app.use('/articles', articles);
-
-app.use((req, res) => {
+app.use(() => {
   throw new NotFoundError('Запрашиваемый ресурс не найден');
 });
 
 app.use(errors());
-
-app.use((err, req, res, next) => {
-  let { statusCode = 500, message } = err;
-
-  if (err.code === 11000 && err.name === 'MongoError') {
-    statusCode = 409;
-    message = 'Пользователь с таким email уже есть';
-  }
-
-  if (err.name === 'ValidationError') {
-    statusCode = 400;
-    message = 'Запрос неверно сформирован';
-  }
-
-  res.status(statusCode).send({
-    message: statusCode === 500
-      ? 'На сервере произошла ошибка'
-      : message,
-  });
-
-  next();
-});
-
+app.use(errorHandler);
 app.use(errorLogger);
 
-start();
+app.listen(PORT);
